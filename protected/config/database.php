d5974b61e2fa569de493e0b2f0217b46aeaf10f8
aleksandr.tarasov <a.tarasov1209@gmail.com>
2017-09-12 14:33:52 +0100

    connected webapp to database

--- protected/config/database.php
+++ protected/config/database.php
@@ -3,12 +3,10 @@
 // This is the database connection configuration.
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-	// uncomment the following lines to use a MySQL database
-	/*
-	'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+//	 uncomment the following lines to use a MySQL database
+	'connectionString' => 'mysql:host=silva.computing.dundee.ac.uk;dbname=ip17team8db',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'ip17team8',
+	'password' => '6593.ip17t.3956',
 	'charset' => 'utf8',
-	*/
 );
