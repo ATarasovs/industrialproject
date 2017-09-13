5317a0743b553787eec3c47d709369cec58be5ff
aleksandr.tarasov <a.tarasov1209@gmail.com>
2017-09-13 12:54:15 +0100

    manage user link added

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -30,7 +30,8 @@
 			'items'=>array(
 				array('label'=>'Dashboard', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Upload', 'url'=>array('/site/contact'), 'visible'=>!Yii::app()->user->getState('isAdmin')),
+				array('label'=>'Upload', 'url'=>array('/site/contact'), 'visible'=>!Yii::app()->user->isGuest),
+                                array('label'=>'User management', 'url'=>array('/user/index'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
