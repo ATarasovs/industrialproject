97316c0490f4102c5c95896788855e1b6d858bc5
aleksandr.tarasov <a.tarasov1209@gmail.com>
2017-09-13 10:04:37 +0100

    added user model, views and controller

---
+++ protected/views/user/_view.php
@@ -0,0 +1,25 @@
+<?php
+/* @var $this UserController */
+/* @var $data User */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('userID')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->userID), array('view', 'id'=>$data->userID)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('username')); ?>:</b>
+	<?php echo CHtml::encode($data->username); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('password')); ?>:</b>
+	<?php echo CHtml::encode($data->password); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('admin')); ?>:</b>
+	<?php echo CHtml::encode($data->admin); ?>
+	<br />
+
+
+</div>
