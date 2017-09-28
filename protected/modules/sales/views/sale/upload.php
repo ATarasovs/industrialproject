ba5716ddebb106719db61060972789a45354b49f
aleksandr.tarasov <a.tarasov1209@gmail.com>
2017-09-28 20:00:21 +0100

    updated upload page

--- protected/modules/sales/views/sale/upload.php
+++ protected/modules/sales/views/sale/upload.php
@@ -1,30 +1,34 @@
-<div class="jumbotron">
-  <h2 class="display-3"><i class="fa fa-cloud-upload" aria-hidden="true"></i> File Upload</h2>
-  <p class="lead">Adding data to the system is easy.</p>
-  <hr class="my-4">
-  <p>To add new data to the system, simply attach the xls file below and click upload! <br/><small>(Also you may use drag & drop function)</small></p>
-  <p class="lead">
-  <br><br>
-    <?php
-    $form = $this->beginWidget(
-        'CActiveForm',
-        array(
-            'id' => 'upload-form',
-            'enableAjaxValidation' => false,
-            'htmlOptions' => array('enctype' => 'multipart/form-data'),
-        )
-    );
-    // ..
-    echo $form->fileField($model, 'file');
-    echo $form->error($model, 'file');
-    // ...
-    ?>
-    <?php
-    echo CHtml::submitButton('Upload', array('class'=>'btn btn-primary'));
-    $this->endWidget();
-    ?>
+<div style="margin-top:3%"class="container" align="center" >
+    <div class="col-md-10" align="center">
+        <div class="jumbotron centered">
+          <h2 class="display-3"><i class="fa fa-cloud-upload" aria-hidden="true"></i> File Upload</h2>
+          <p class="lead">Adding data to the system is easy.</p>
+          <hr class="my-4">
+          <p>To add new data to the system, simply attach the xls file below and click upload! <br/><small>(Also you may use drag & drop function)</small></p>
+          <p class="lead">
+          <br><br>
+            <?php
+            $form = $this->beginWidget(
+                'CActiveForm',
+                array(
+                    'id' => 'upload-form',
+                    'enableAjaxValidation' => false,
+                    'htmlOptions' => array('enctype' => 'multipart/form-data'),
+                )
+            );
+            // ..
+            echo $form->fileField($model, 'file');
+            echo $form->error($model, 'file');
+            // ...
+            ?>
+            <?php
+            echo CHtml::submitButton('Upload', array('class'=>'btn btn-primary'));
+            $this->endWidget();
+            ?>
 
-  </p>
+          </p>
+        </div>
+    </div>
 </div>
 
 
