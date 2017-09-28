37983a182208dbc5967756aa3ea90ffde554132b
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2017-09-28 13:52:04 +0100

    Fixed db connection

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,22 +1,22 @@
 <?php
 
-// // This is the database connection configuration.
-// return array(
-// 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-// //	 uncomment the following lines to use a MySQL database
-// 	'connectionString' => 'mysql:host=silva.computing.dundee.ac.uk;dbname=ip17team8db',
-// 	'emulatePrepare' => true,
-// 	'username' => 'ip17team8',
-// 	'password' => '6593.ip17t.3956',
-// 	'charset' => 'utf8',
-// );
-
+// This is the database connection configuration.
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 //	 uncomment the following lines to use a MySQL database
-	'connectionString' => 'mysql:host=localhost;dbname=team8',
+	'connectionString' => 'mysql:host=silva.computing.dundee.ac.uk;dbname=ip17team8db',
 	'emulatePrepare' => true,
-	'username' => 'root',
-	'password' => '',
+	'username' => 'ip17team8',
+	'password' => '6593.ip17t.3956',
 	'charset' => 'utf8',
 );
+
+// return array(
+// 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+// //	 uncomment the following lines to use a MySQL database
+// 	'connectionString' => 'mysql:host=localhost;dbname=team8',
+// 	'emulatePrepare' => true,
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'charset' => 'utf8',
+// );
