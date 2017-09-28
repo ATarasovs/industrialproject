0a6c35b735945da0d8351a5c68c0dc81894ff28e
Gary Gillespie <gary-gillespie@hotmail.co.uk>
2017-09-28 13:40:50 +0100

    Fixed accessibility mode, changed chart init values, changed chart colour scheme, changed quickview layout

--- protected/config/database.php
+++ protected/config/database.php
@@ -1,22 +1,22 @@
 <?php
 
-// This is the database connection configuration.
+// // This is the database connection configuration.
+// return array(
+// 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+// //	 uncomment the following lines to use a MySQL database
+// 	'connectionString' => 'mysql:host=silva.computing.dundee.ac.uk;dbname=ip17team8db',
+// 	'emulatePrepare' => true,
+// 	'username' => 'ip17team8',
+// 	'password' => '6593.ip17t.3956',
+// 	'charset' => 'utf8',
+// );
+
 return array(
 	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 //	 uncomment the following lines to use a MySQL database
-	'connectionString' => 'mysql:host=silva.computing.dundee.ac.uk;dbname=ip17team8db',
+	'connectionString' => 'mysql:host=localhost;dbname=team8',
 	'emulatePrepare' => true,
-	'username' => 'ip17team8',
-	'password' => '6593.ip17t.3956',
+	'username' => 'root',
+	'password' => '',
 	'charset' => 'utf8',
 );
-
-//return array(
-//	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-////	 uncomment the following lines to use a MySQL database
-//	'connectionString' => 'mysql:host=localhost;dbname=mydb',
-//	'emulatePrepare' => true,
-//	'username' => 'root',
-//	'password' => 'root',
-//	'charset' => 'utf8',
-//);
