a1b91335992092f4c4d7e187475dcfe5bb4b59ce
aleksandr.tarasov <a.tarasov1209@gmail.com>
2017-09-29 11:37:27 +0100

    added videos on help page and change structure of page

--- protected/modules/sales/views/tutorial/index.php
+++ protected/modules/sales/views/tutorial/index.php
@@ -2,12 +2,21 @@
     <div class="col-md-10" align="center">
         <div class="jumbotron centered">
             <h1><i class="fa fa-question-circle" aria-hidden="true"></i> Tutorial</h1><br/>
-            <iframe width="640px" height="400" src="https://www.youtube.com/embed/XGSy3_Czz8k"></iframe>
-            <div class="col-6">
-                <br/> 
+            <div class="col-8">
                 <form action="https://drive.google.com/file/d/0B1y3r0LCfa0kRXRXaFJXMG9lRDQ/view?usp=sharing">
-                    <button class = "btn btn-lg btn-primary btn-block" id ="backBtn">View user manual</button>
+                    <button class = "btn btn-lg btn-primary btn-block" id ="backBtn">View user manual</button><BR/>
                 </form>
+            </div>
+            <h4><i class="fa fa-youtube-play" aria-hidden="true"></i> DUSA Analytics tutorial: Dashboard</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/cDTYLNWH_cc" frameborder="0" allowfullscreen></iframe><br/><br/>
+            <h4><i class="fa fa-youtube-play" aria-hidden="true"></i> DUSA Analytics tutorial: Sales</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/qK8JtcXTfCo" frameborder="0" allowfullscreen></iframe><br/><br/>
+            <h4><i class="fa fa-youtube-play" aria-hidden="true"></i> DUSA Analytics tutorial: Users</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/JeJHb3K8keU" frameborder="0" allowfullscreen></iframe><br/><br/>
+            <h4><i class="fa fa-youtube-play" aria-hidden="true"></i> DUSA Analytics tutorial: Upload files</h4>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/oN_pxOsS8kk" frameborder="0" allowfullscreen></iframe><br/><br/>
+            <div class="col-6">
+                <br/>
             </div>
         </div>
     </div>
